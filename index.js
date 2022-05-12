cfd4752ab901fdcec6ba5b76ef1343015581ebee
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2022-05-12 18:05:00 -0300

    formulario1

---
+++ index.js
@@ -0,0 +1,13 @@
+function cadastro(){
+  var nomeusuario = document.getElementById('usuario')
+  var emailcad = document.getElementById('email')
+  var senhacad = document.getElementById('senha')
+  var confirmacaocad = document.getElementById('confirmacao')
+  var botaocad = document.getElementById('cadastro-botao')
+
+
+  
+}
+
+
+
